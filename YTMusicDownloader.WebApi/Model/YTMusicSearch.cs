98216927daa2f71b74d317358e059359447b95d3
Egor Konovalov <konovalov.ye@gmail.com>
2022-10-10 09:18:59 +0400

    added service for web-view

---
+++ YTMusicDownloader.WebApi/Model/YTMusicSearch.cs
@@ -0,0 +1,9 @@
+﻿namespace YTMusicDownloader.WebApi.Model
+{
+    public class YTMusicSearchResult
+    {
+        public string YouTubeMusicPlaylistUrl { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ YTMusicDownloader.WebApi/Services/TelegramService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+using YTMusicDownloader.WebApi.Model;
+
+namespace YTMusicDownloader.WebApi.Services
+{
+    public class TelegramService : ITelegramService
+    {
+        private readonly YoutubeClient _youtubeClient;
+
+        private const int ElementsPerPageCount = 8;
+
+        public TelegramService()
+        {
+            _youtubeClient = new YoutubeClient();
+        }
+
+        public async Task<IEnumerable<YTMusicSearchResult>> Search(string query, int page, CancellationToken cancellationToken)
+        {
+            return (await _youtubeClient.Search
+                .GetPlaylistsAsync(query, cancellationToken))
+                .Skip(ElementsPerPageCount * page)
+                .Take(ElementsPerPageCount)
+                .Select(result => new YTMusicSearchResult
+                {
+                    ImageUrl = result.Thumbnails.Last().Url,
+                    Title = result.Title,
+                    YouTubeMusicPlaylistUrl = result.Url
+                }).ToList();
+        }
+    }
+}
