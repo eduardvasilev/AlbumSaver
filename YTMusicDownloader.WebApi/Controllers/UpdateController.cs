bb7968d036e81573381335389d5d3e7add7a5e85
Eduard Vasilev <daetozheed@gmail.com>
2024-06-18 00:26:13 +0700

    Added stars feature

--- YTMusicDownloader.WebApi/Controllers/UpdateController.cs
+++ YTMusicDownloader.WebApi/Controllers/UpdateController.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Requests.Abstractions;
 using Telegram.Bot.Types;
 using YTMusicDownloader.WebApi.Services;
 
@@ -11,19 +15,39 @@ namespace YTMusicDownloader.WebApi.Controllers
     public class UpdateController : Controller
     {
         private readonly IUpdateService _updateService;
+        private readonly IBotService _botService;
 
-        public UpdateController(IUpdateService updateService)
+        public UpdateController(IUpdateService updateService, IBotService botService)
         {
             _updateService = updateService;
+            _botService = botService;
         }
 
         // POST api/update
         [HttpPost]
 
-        public async Task<IActionResult> Post([FromBody] Update update, CancellationToken cancellationToken)
+        public async Task<IActionResult> Post([FromBody] Update request, CancellationToken cancellationToken)
         {
+            Update? update;
+            try
+            {
+                update = JsonConvert.DeserializeObject<Update>(request.ToString());
+
+            }
+            catch (Exception)
+            {
+                return Ok();
+            }
+
+            if (update is { PreCheckoutQuery: { } })
+            {
+                var preCheckoutQuery = update.PreCheckoutQuery;
+                await _botService.Client.AnswerPreCheckoutQueryAsync(
+                    preCheckoutQueryId: preCheckoutQuery.Id, cancellationToken: cancellationToken);
+                await _botService.Client.SendTextMessageAsync(-911492578, $"Donate from @{preCheckoutQuery.From.Username}: \n\r{preCheckoutQuery.TotalAmount} {preCheckoutQuery.Currency}", cancellationToken: cancellationToken);
+
+            }
             //call and forget
-            _updateService.ProcessAsync(update, cancellationToken);
             return Ok();
         }
 
