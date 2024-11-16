c3be4aa9f8c8d1aa41e6f1343c636485239183b3
Eduard Vasilev <daetozheed@gmail.com>
2024-11-16 20:54:43 +0700

    Added redis cache

---
+++ YTMusicDownloader.WebApi/Services/Telegram/TelegramFilesService.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace YTMusicDownloader.WebApi.Services.Telegram;
+
+public class TelegramFilesService : ITelegramFilesService
+{
+    private IDatabase _db;
+
+    public TelegramFilesService()
+    {
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("host.docker.internal:6379");
+        _db = redis.GetDatabase();
+    }
+
+    public async Task<string> GetFileIdAsync(string trackUrl)
+    {
+        return await _db.StringGetAsync(trackUrl);
+    }
+
+    public async Task SetFileIdAsync(string trackUrl, string fileId)
+    {
+        await _db.StringSetAsync(trackUrl, fileId);
+    }
+}
