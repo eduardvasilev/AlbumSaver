93e4dec6bc7234809e06ce37da6c289e9fa24f04
Eduard Vasilev <daetozheed@gmail.com>
2025-02-06 18:43:17 +0700

    test

--- YTMusicDownloader.WebApi/Services/DownloadService2.cs
+++ YTMusicDownloader.WebApi/Services/DownloadService2.cs
@@ -136,6 +136,18 @@ public class DownloadService2 : IDownloadService
             author = author.Substring(0, author.Length - topic.Length);
         }
 
+        string fileId = await _telegramFilesService.GetFileIdAsync(track.Url);
+
+        if (fileId != null)
+        {
+            await _botService.Client.SendAudioAsync(chatId, InputFile.FromFileId(fileId),
+                    cancellationToken: CancellationToken.None,
+                    duration: (track.Duration.HasValue ? (int?)track.Duration.Value.TotalSeconds : null),
+                    parseMode: ParseMode.Html, thumbnail: thump, title: track.Title, disableNotification: true, performer: track.Author.ChannelTitle);
+
+            return;
+        }
+
         await using Stream stream = await GetAudioStreamAsync(track.Id, cancellationToken);
 
         var sendAudioAsync = await _botService.Client.SendAudio(chatId, new InputFileStream(stream, track.Title),
@@ -144,6 +156,7 @@ public class DownloadService2 : IDownloadService
             parseMode: ParseMode.Html, thumbnail: thump, title: track.Title, disableNotification: true,
             performer: author, protectContent: false);
 
+        await _telegramFilesService.SetFileIdAsync(track.Url, sendAudioAsync.Audio.FileId);
     }
 
     public async Task<Stream> GetAudioStreamAsync(VideoId videoId, CancellationToken cancellationToken)
